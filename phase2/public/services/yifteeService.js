9c193483c97dcc7629352c1183243682b0a249b8
Nelson <nelson.alemar@upr.edu>
2017-01-30 03:44:33 -0400

    GET Request finally works. But search query doesn't for some odd reason

--- phase2/public/services/yifteeService.js
+++ phase2/public/services/yifteeService.js
@@ -4,8 +4,9 @@ angular.module('users')
 function($http) {
 	var yifteeService = {};
 	yifteeService.getUsers = function (input) {
-		return $http.get("http://yifteeqa.com/programming_challenge?q="+input)
+		return $http.get("http://yifteeqa.com/programming_challenge/autocomplete?q="+input)
 		.then(function (response) {
+		    console.log(response);
 			return response.data;
 		})
 		.then(null, function (err) {
--- phase2/public/src/users/UserController.js
+++ phase2/public/src/users/UserController.js
@@ -1,18 +1,22 @@
 var app = angular.module("users")
-    .controller('UserController',['yifteeService','$mdSidenav','$mdBottomSheet', '$timeout', '$log', '$scope', '$mdDialog', '$location', '$q', '$route', function(yifteeService, $mdSidenav, $mdBottomSheet, $timeout, $log, $scope, $mdDialog, $location, $q, $route)
+    .controller('UserController',['$mdSidenav','$mdBottomSheet', '$timeout', '$log', '$scope', '$mdDialog', '$location', '$q', 'yifteeService', function($mdSidenav, $mdBottomSheet, $timeout, $log, $scope, $mdDialog, $location, $q, yifteeService)
     {
 
     var self = this;
 
-    $scope.user = {'name': "",
+    $scope.tempUser = {'name': "",
                     'email': ""};
-    $scope.recipient = {'name': "",
+    $scope.tempRecipient = {'name': "",
                         'sms': "",
                         'email': "",
                         'message': ""};
 
+    $scope.user = $scope.tempUser;
+    $scope.recipient = $scope.tempRecipient;
+
     $scope.sms = false;
     $scope.email = false;
+
 
     $scope.validateEmail = function(input){
 
@@ -94,30 +98,34 @@ var app = angular.module("users")
     self.names = [];
     self.sms = [];
     self.s = [];
+    self.simulateQuery = false;
 
     self.nameSearch = function(input){
 
         console.log(input);
-        yifteeService.getUsers(input)
-            .then(function(response){
+        yifteeService.getUsers(input).then(function(response){
 
-                self.users = response.contacts;
+                self.users = response.response.contacts;
 
             });
 
         self.names = loadNames();
+        console.log(self.names);
+        var results = input ? self.names.filter( createFilterFor(input) ) : self.names, deferred;
 
-        var results = query ? self.names.filter( createFilterFor(input) ) : self.names;
-        var deferred = $q.defer();
-        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-        return deferred.promise;
+         if (self.simulateQuery){
+            deferred = $q.defer();
+            $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+            return deferred.promise;}
+
+        else
+            return results;
 
     }
 
     function loadNames(){
 
         var names = self.users.map(function (user){
-
             return user.name;
         })
 
@@ -133,16 +141,21 @@ var app = angular.module("users")
             yifteeService.getUsers(input)
                 .then(function(response){
 
-                    self.users = response.contacts;
+                    self.users = response.response.contacts;
 
                 });
 
             self.sms = loadSMS();
 
-            var results = query ? self.sms.filter( createFilterFor(input) ) : self.sms;
-            var deferred = $q.defer();
-            $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-            return deferred.promise;
+            var results = input ? self.sms.filter( createFilterFor(input) ) : self.sms, deferred;
+
+           if (self.simulateQuery){
+               deferred = $q.defer();
+               $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+               return deferred.promise;}
+
+           else
+               return results;
 
         }
 
@@ -157,26 +170,31 @@ var app = angular.module("users")
 
         }
 
-     self.Search = function(input){
+     self.emailSearch = function(input){
 
                 console.log(input);
                 yifteeService.getUsers(input)
                     .then(function(response){
 
-                        self.users = response.contacts;
+                        self.users = response.response.contacts;
 
                     });
 
                 self.emails = loadEmails();
 
-                var results = query ? self.emails.filter( createFilterFor(input) ) : self.emails;
-                var deferred = $q.defer();
-                $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-                return deferred.promise;
+                var results = input ? self.emails.filter( createFilterFor(input) ) : self.emails;
+
+                if (self.simulateQuery){
+                            deferred = $q.defer();
+                            $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+                            return deferred.promise;}
+
+                        else
+                            return results;
 
             }
 
-            function loadSMS(){
+            function loadEmails(){
 
                 var emails = self.users.map(function (user){
 
@@ -192,7 +210,7 @@ var app = angular.module("users")
                  var lowercaseQuery = angular.lowercase(query);
 
                  return function filterFn(state) {
-                   return (state.value.indexOf(lowercaseQuery) === 0);
+                   return (state.value.indexOf(lowercaseQuery) != -1);
                  };
 
                }
@@ -200,8 +218,12 @@ var app = angular.module("users")
 
         $scope.validate = function (){
 
+//            console.log("sms: "+$scope.sms);
+//            console.log("email: "+$scope.email);
+//            console.log($scope.recipient);
+//            console.log($scope.user);
 
-            if($scope.sms === true && $scope.recipient.name.length > 1 && $scope.recipient.message.length > 1 && $scope.user.email.length > 1){
+            if($scope.sms === true && $scope.recipient.name.length > 1 && $scope.recipient.message.length >= 1 && $scope.user.email.length > 1){
 
                 return true;
 
@@ -216,6 +238,37 @@ var app = angular.module("users")
 
             else
                 return false;
+
+        }
+
+        $scope.saveName = function(param){
+
+            $scope.recipient.name = param;
+        }
+
+        $scope.savePhone = function(param){
+
+            $scope.recipient.phone = param;
+        }
+
+        $scope.saveEmail = function(param){
+
+            $scope.recipient.email = param;
+        }
+
+        $scope.saveMessage = function(param){
+
+            $scope.recipient.message = param;
+        }
+
+
+        $scope.success = function(){
+
+            swal(
+                        'Sent',
+                        'Message was sent',
+                        'success'
+                      );
 
         }
 
